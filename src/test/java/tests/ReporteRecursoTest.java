993f3aa6bd951e09aff48887928a0cbba9cfac97
Laura Ramos <lauramilena.rbe@gmail.com>
2017-04-09 18:56:12 -0500

    Creacion xhtml

--- src/test/java/tests/ReporteRecursoTest.java
+++ src/test/java/tests/ReporteRecursoTest.java
@@ -23,15 +23,35 @@ public class ReporteRecursoTest {
     public ReporteRecursoTest() {
     }
     
-    
+
     @Before
     public void setUp() {
     }
+    /**
+     * Seleccion de peridodo academico para generar el reporte de recursos para 
+     * un programa de posgrado
+     * Clase Equivalencia
+     * CE1: Recursos del mismo perirodo seleccionado.
+     * @throws ExceptionServiciosReporte 
+     */
+    @Test
+    public void CE1Test() throws ExceptionServiciosReporte{
+        assertTrue(true);     
+    }
     
+    /**
+     * Seleccion de peridodo academico para generar el reporte de recursos para 
+     * un programa de posgrado
+     * Clase de Frontera
+     * CF1: Recursos del actual periodo academico.
+     * @throws ExceptionServiciosReporte 
+     */
     @Test
     public void CF1Test() throws ExceptionServiciosReporte{
         assertTrue(true);     
     }
+    
+        
     @After
     public void tearDown() {
     }
