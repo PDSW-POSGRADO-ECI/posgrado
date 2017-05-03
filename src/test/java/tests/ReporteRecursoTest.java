581ed115e376b9578c0d6d6df18ca814a38e63fa
Laura Ramos <lauramilena.rbe@gmail.com>
2017-05-02 20:25:51 -0500

    pruebas de recursos arregladas para la nueva base de datos y despliegue

--- src/test/java/tests/ReporteRecursoTest.java
+++ src/test/java/tests/ReporteRecursoTest.java
@@ -37,6 +37,7 @@ public class ReporteRecursoTest {
     @After 
     public void clearDB() throws SQLException, Exception{
         Statement stmt = getConnection().createStatement();
+        stmt.execute("delete from RecursoClase");
         stmt.execute("delete from Recurso");
         stmt.execute("delete from Clase");
         stmt.execute("delete from MateriaCohorte");
@@ -46,6 +47,7 @@ public class ReporteRecursoTest {
         stmt.execute("delete from Asignatura");
         stmt.execute("delete from Posgrado");
         stmt.execute("delete from Periodo");
+        
         getConnection().commit();
         getConnection().close();
     }
@@ -61,7 +63,7 @@ public class ReporteRecursoTest {
      * @throws java.sql.SQLException */
      
     @Test
-    public void CERecursos1Test() throws ExceptionServiciosReporte,SQLException, Exception{
+    public void CERecursos1Test() throws ExceptionServiciosReporte,SQLException{
         Statement stmt=getConnection().createStatement(); 
         stmt.execute("INSERT INTO Posgrado(id, nombre,creditos )  VALUES(1, 'Economias',100 );");
         stmt.execute("INSERT INTO Asignatura (id, nombre,posgrado_id )  VALUES(1, 'Propuesta Elementales',1 );");
@@ -72,12 +74,14 @@ public class ReporteRecursoTest {
         stmt.execute("INSERT INTO Materia (sigla, nombre,creditos,asignatura_id,descripcion )  VALUES ( 'FGPR', 'fundamentos gerenciales',1,1,'fundamentos de gerencia de proyectos para empresarios' );");
         stmt.execute("INSERT INTO Profesor (documento, nombre,correo,telefono,tipo_documento )  VALUES (1418432, 'Crisrtian Alba', 'sergo@correo.com',8165134,'cc' );");
         stmt.execute("INSERT INTO Profesor (documento, nombre,correo,telefono,tipo_documento )  VALUES (1818428, 'Sergio Chacon', 'sergio@coreo.com',3115134,'cc' );");
-        stmt.execute("INSERT INTO MateriaCohorte (materia_sigla, cohorte_id,profesor_documento) VALUES (  'FGPR', 4,1818428)");
-        stmt.execute("INSERT INTO MateriaCohorte (materia_sigla, cohorte_id,profesor_documento) VALUES (  'FGPR',24,1418432)");
+        stmt.execute("INSERT INTO MateriaCohorte (materia_sigla, cohorte_id,profesor_documento) VALUES (  'FGPR', 4,1818428);");
+        stmt.execute("INSERT INTO MateriaCohorte (materia_sigla, cohorte_id,profesor_documento) VALUES (  'FGPR',24,1418432);");
         stmt.execute("INSERT INTO Clase (id,hora_inicio,hora_fin,fecha,materiacohorte_materia_sigla,materiacohorte_cohorte_id ,materiacohorte_profesor_documento)  VALUES(1,'7:00:00','10:00:00', '2017-01-02', 'FGPR', 4,1818428);");
         stmt.execute("INSERT INTO Clase (id,hora_inicio,hora_fin,fecha,materiacohorte_materia_sigla,materiacohorte_cohorte_id ,materiacohorte_profesor_documento)  VALUES(2,'15:00:00','17:00:00', '2017-01-01', 'FGPR',24,1418432);");
-        stmt.execute("INSERT INTO Recurso (id,recurso,disponible,clase_id ,cantidad)  VALUES(1, 'libro de economia 1',true, 2,1);");
-        stmt.execute("INSERT INTO Recurso (id,recurso,disponible,clase_id ,cantidad)  VALUES(2, 'libro de economia 1',true, 2,1);");
+        stmt.execute("INSERT INTO Recurso (id,recurso,disponible ,cantidad)  VALUES(1, 'libro de economia 1',true,10);");
+        stmt.execute("INSERT INTO Recurso (id,recurso,disponible,cantidad)  VALUES(2, 'libro de economia 1',true,10);");
+        stmt.execute("INSERT INTO RecursoClase (clase_id,recurso_id,cantidadUSE)  VALUES(2,1,10);");
+        stmt.execute("INSERT INTO RecursoClase (clase_id,recurso_id,cantidadUSE)  VALUES(2,2,10);");
         getConnection().close();
         ServiciosReporte report=ServiciosReporteFactory.getInstance().getServiciosReporteForTesting();
         List<Recurso> rec=report.consultarRecursosXperiodo(report.obtenerPeriodos().get(0));
@@ -93,7 +97,7 @@ public class ReporteRecursoTest {
      * @throws ExceptionServiciosReporte */
      
     @Test
-    public void CERecursos2Test() throws ExceptionServiciosReporte, SQLException, Exception{
+    public void CERecursos2Test() throws ExceptionServiciosReporte, SQLException{
         Statement stmt=getConnection().createStatement(); 
         stmt.execute("INSERT INTO Posgrado(id, nombre,creditos )  VALUES(1, 'Economias',100 );");
         stmt.execute("INSERT INTO Asignatura (id, nombre,posgrado_id )  VALUES(1, 'Propuesta Elementales',1 );");
@@ -104,15 +108,18 @@ public class ReporteRecursoTest {
         stmt.execute("INSERT INTO Materia (sigla, nombre,creditos,asignatura_id,descripcion )  VALUES ( 'FGPR', 'fundamentos gerenciales',1,1,'fundamentos de gerencia de proyectos para empresarios' );");
         stmt.execute("INSERT INTO Profesor (documento, nombre,correo,telefono,tipo_documento )  VALUES (1418432, 'Crisrtian Alba', 'sergo@correo.com',8165134,'cc' );");
         stmt.execute("INSERT INTO Profesor (documento, nombre,correo,telefono,tipo_documento )  VALUES (1818428, 'Sergio Chacon', 'sergio@coreo.com',3115134,'cc' );");
-        stmt.execute("INSERT INTO MateriaCohorte (materia_sigla, cohorte_id,profesor_documento) VALUES (  'FGPR', 4,1818428)");
-        stmt.execute("INSERT INTO MateriaCohorte (materia_sigla, cohorte_id,profesor_documento) VALUES ( 'FGPR',24,1418432)");
+        stmt.execute("INSERT INTO MateriaCohorte (materia_sigla, cohorte_id,profesor_documento) VALUES (  'FGPR', 4,1818428);");
+        stmt.execute("INSERT INTO MateriaCohorte (materia_sigla, cohorte_id,profesor_documento) VALUES ( 'FGPR',24,1418432);");
         stmt.execute("INSERT INTO Clase (id,hora_inicio,hora_fin,fecha,materiacohorte_materia_sigla,materiacohorte_cohorte_id ,materiacohorte_profesor_documento)  VALUES(1,'7:00:00','10:00:00', '2017-01-02', 'FGPR', 4,1818428);");
         stmt.execute("INSERT INTO Clase (id,hora_inicio,hora_fin,fecha,materiacohorte_materia_sigla,materiacohorte_cohorte_id ,materiacohorte_profesor_documento)  VALUES(2,'15:00:00','17:00:00', '2017-01-01', 'FGPR',24,1418432);");
-        stmt.execute("INSERT INTO Recurso (id,recurso,disponible,clase_id ,cantidad)  VALUES(1, 'libro de economia 1',true, 1,1);");
-        stmt.execute("INSERT INTO Recurso (id,recurso,disponible,clase_id ,cantidad)  VALUES(2, 'libro de economia 1',true, 1,1);");
-        stmt.execute("INSERT INTO Recurso (id,recurso,disponible,clase_id ,cantidad)  VALUES(4, 'libro de economia 2',true, 2,11);");
-        stmt.execute("INSERT INTO Recurso (id,recurso,disponible,clase_id ,cantidad)  VALUES(3, 'libro de economia 1',true, 1,10);");
-        
+        stmt.execute("INSERT INTO Recurso (id,recurso,disponible,cantidad)  VALUES(1, 'libro de economia 1',true,1);");
+        stmt.execute("INSERT INTO Recurso (id,recurso,disponible ,cantidad)  VALUES(2, 'libro de economia 1',true,1);");
+        stmt.execute("INSERT INTO Recurso (id,recurso,disponible ,cantidad)  VALUES(4, 'libro de economia 2',true,11);");
+        stmt.execute("INSERT INTO Recurso (id,recurso,disponible ,cantidad)  VALUES(3, 'libro de economia 1',true,10);");
+        stmt.execute("INSERT INTO RecursoClase (clase_id,recurso_id,cantidadUSE)  VALUES(1,1,10);");
+        stmt.execute("INSERT INTO RecursoClase (clase_id,recurso_id,cantidadUSE)  VALUES(1,2,10);");
+        stmt.execute("INSERT INTO RecursoClase (clase_id,recurso_id,cantidadUSE)  VALUES(2,4,10);");
+        stmt.execute("INSERT INTO RecursoClase (clase_id,recurso_id,cantidadUSE)  VALUES(1,3,10);");
         ServiciosReporte report=ServiciosReporteFactory.getInstance().getServiciosReporteForTesting();
         List<Recurso> rec=report.consultarRecursosXperiodo(report.obtenerPeriodos().get(0));
         assertEquals("Deberian devolver los recursos del periodo 2017-1",rec.size(),1);
@@ -128,7 +135,7 @@ public class ReporteRecursoTest {
      * @throws java.sql.SQLException */
      
     @Test
-    public void CERecursos3Test() throws ExceptionServiciosReporte, SQLException, Exception{
+    public void CERecursos3Test() throws ExceptionServiciosReporte, SQLException{
         Statement stmt = getConnection().createStatement();
         stmt.execute("INSERT INTO Posgrado(id, nombre,creditos )  VALUES(1, 'Economias',100 );");
         stmt.execute("INSERT INTO Asignatura (id, nombre,posgrado_id )  VALUES(1, 'Propuesta Elementales',1 );");
@@ -139,13 +146,14 @@ public class ReporteRecursoTest {
         stmt.execute("INSERT INTO Materia (sigla, nombre,creditos,asignatura_id,descripcion )  VALUES ( 'FGPR', 'fundamentos gerenciales',1,1,'fundamentos de gerencia de proyectos para empresarios' );");
         stmt.execute("INSERT INTO Profesor (documento, nombre,correo,telefono,tipo_documento )  VALUES (1418432, 'Crisrtian Alba', 'sergo@correo.com',8165134,'cc' );");
         stmt.execute("INSERT INTO Profesor (documento, nombre,correo,telefono,tipo_documento )  VALUES (1818428, 'Sergio Chacon', 'sergio@coreo.com',3115134,'cc' );");
-        stmt.execute("INSERT INTO MateriaCohorte (materia_sigla, cohorte_id,profesor_documento) VALUES ( 'FGPR', 4,1818428)");
-        stmt.execute("INSERT INTO MateriaCohorte (materia_sigla, cohorte_id,profesor_documento) VALUES ( 'FGPR',24,1418432)");
+        stmt.execute("INSERT INTO MateriaCohorte (materia_sigla, cohorte_id,profesor_documento) VALUES ( 'FGPR', 4,1818428);");
+        stmt.execute("INSERT INTO MateriaCohorte (materia_sigla, cohorte_id,profesor_documento) VALUES ( 'FGPR',24,1418432);");
         stmt.execute("INSERT INTO Clase (id,hora_inicio,hora_fin,fecha,materiacohorte_materia_sigla,materiacohorte_cohorte_id ,materiacohorte_profesor_documento)  VALUES(1,'7:00:00','10:00:00', '2017-01-02', 'FGPR', 4,1818428);");
         stmt.execute("INSERT INTO Clase (id,hora_inicio,hora_fin,fecha,materiacohorte_materia_sigla,materiacohorte_cohorte_id ,materiacohorte_profesor_documento)  VALUES(2, '15:00:00','17:00:00', '2017-01-01', 'FGPR',24,1418432);");
-        stmt.execute("INSERT INTO Recurso (id,recurso,disponible,clase_id ,cantidad)  VALUES(1, 'libro de economia 1',true, 2,1);");
-        stmt.execute("INSERT INTO Recurso (id,recurso,disponible,clase_id ,cantidad)  VALUES(2, 'libro de economia 1',true, 2,1);");
-          
+        stmt.execute("INSERT INTO Recurso (id,recurso,disponible,cantidad)  VALUES(1, 'libro de economia 1',true,40);");
+        stmt.execute("INSERT INTO Recurso (id,recurso,disponible ,cantidad)  VALUES(2, 'libro de economia 1',true,40);");
+        stmt.execute("INSERT INTO RecursoClase (clase_id,recurso_id,cantidadUSE)  VALUES(2,1,10);");
+        stmt.execute("INSERT INTO RecursoClase (clase_id,recurso_id,cantidadUSE)  VALUES(2,2,10);");
         ServiciosReporte report=ServiciosReporteFactory.getInstance().getServiciosReporteForTesting();
         List<Recurso> rec=report.consultarRecursosXperiodo(report.obtenerPeriodos().get(0));
         assertEquals("Deberian devolver los recursos del periodo 2017-1",rec.get(0).getClase_id().getFecha(),report.colsultarFechas(report.obtenerPeriodos().get(0)).get(0));
@@ -161,7 +169,7 @@ public class ReporteRecursoTest {
      * @throws java.sql.SQLException */
      
     @Test
-    public void CERecursos5Test() throws ExceptionServiciosReporte, SQLException, Exception{
+    public void CERecursos5Test() throws ExceptionServiciosReporte, SQLException{
         Statement stmt = getConnection().createStatement();
         stmt.execute("INSERT INTO Posgrado(id, nombre,creditos )  VALUES(1, 'Economias',100 );");
         stmt.execute("INSERT INTO Asignatura (id, nombre,posgrado_id )  VALUES(1, 'Propuesta Elementales',1 );");
@@ -172,8 +180,8 @@ public class ReporteRecursoTest {
         stmt.execute("INSERT INTO Materia (sigla, nombre,creditos,asignatura_id,descripcion )  VALUES ( 'FGPR', 'fundamentos gerenciales',1,1,'fundamentos de gerencia de proyectos para empresarios' );");
         stmt.execute("INSERT INTO Profesor (documento, nombre,correo,telefono,tipo_documento )  VALUES (1418432, 'Crisrtian Alba', 'sergo@correo.com',8165134,'cc' );");
         stmt.execute("INSERT INTO Profesor (documento, nombre,correo,telefono,tipo_documento )  VALUES (1818428, 'Sergio Chacon', 'sergio@coreo.com',3115134,'cc' );");
-        stmt.execute("INSERT INTO MateriaCohorte (materia_sigla, cohorte_id,profesor_documento) VALUES ( 'FGPR', 4,1818428)");
-        stmt.execute("INSERT INTO MateriaCohorte (materia_sigla, cohorte_id,profesor_documento) VALUES ( 'FGPR',24,1418432)");
+        stmt.execute("INSERT INTO MateriaCohorte (materia_sigla, cohorte_id,profesor_documento) VALUES ( 'FGPR', 4,1818428);");
+        stmt.execute("INSERT INTO MateriaCohorte (materia_sigla, cohorte_id,profesor_documento) VALUES ( 'FGPR',24,1418432);");
         stmt.execute("INSERT INTO Clase (id,hora_inicio,hora_fin,fecha,materiacohorte_materia_sigla,materiacohorte_cohorte_id ,materiacohorte_profesor_documento)  VALUES(1,'7:00:00','10:00:00', '2017-01-02', 'FGPR', 4,1818428);");
         stmt.execute("INSERT INTO Clase (id,hora_inicio,hora_fin,fecha,materiacohorte_materia_sigla,materiacohorte_cohorte_id ,materiacohorte_profesor_documento)  VALUES(2,'15:00:00','17:00:00', '2017-01-01', 'FGPR',24,1418432);");
         //stmt.execute("INSERT INTO Recurso (id,recurso,disponible,clase_id ,cantidad)  VALUES(1, 'libro de economia 1',true, 1,1);");
@@ -194,7 +202,7 @@ public class ReporteRecursoTest {
      * @throws java.sql.SQLException */
      
     @Test
-    public void CERecursos4Test() throws ExceptionServiciosReporte, SQLException, Exception{
+    public void CERecursos4Test() throws ExceptionServiciosReporte, SQLException{
         Statement stmt=getConnection().createStatement(); 
         stmt.execute("INSERT INTO Posgrado(id, nombre,creditos )  VALUES(1, 'Economias',100 );");
         stmt.execute("INSERT INTO Asignatura (id, nombre,posgrado_id )  VALUES(1, 'Propuesta Elementales',1 );");
@@ -205,13 +213,14 @@ public class ReporteRecursoTest {
         stmt.execute("INSERT INTO Materia (sigla, nombre,creditos,asignatura_id,descripcion )  VALUES ( 'FGPR', 'fundamentos gerenciales',1,1,'fundamentos de gerencia de proyectos para empresarios' );");
         stmt.execute("INSERT INTO Profesor (documento, nombre,correo,telefono,tipo_documento )  VALUES (1418432, 'Crisrtian Alba', 'sergo@correo.com',8165134,'cc' );");
         stmt.execute("INSERT INTO Profesor (documento, nombre,correo,telefono,tipo_documento )  VALUES (1818428, 'Sergio Chacon', 'sergio@coreo.com',3115134,'cc' );");
-        stmt.execute("INSERT INTO MateriaCohorte (materia_sigla, cohorte_id,profesor_documento) VALUES ( 'FGPR', 4,1818428)");
-        stmt.execute("INSERT INTO MateriaCohorte (materia_sigla, cohorte_id,profesor_documento) VALUES ( 'FGPR',24,1418432)");
+        stmt.execute("INSERT INTO MateriaCohorte (materia_sigla, cohorte_id,profesor_documento) VALUES ( 'FGPR', 4,1818428);");
+        stmt.execute("INSERT INTO MateriaCohorte (materia_sigla, cohorte_id,profesor_documento) VALUES ( 'FGPR',24,1418432);");
         stmt.execute("INSERT INTO Clase (id,hora_inicio,hora_fin,fecha,materiacohorte_materia_sigla,materiacohorte_cohorte_id ,materiacohorte_profesor_documento)  VALUES(1,'7:00:00','10:00:00', '2017-01-02', 'FGPR', 4,1818428);");
         stmt.execute("INSERT INTO Clase (id,hora_inicio,hora_fin,fecha,materiacohorte_materia_sigla,materiacohorte_cohorte_id ,materiacohorte_profesor_documento)  VALUES(2, '15:00:00','17:00:00', '2017-01-01', 'FGPR',24,1418432);");
-        stmt.execute("INSERT INTO Recurso (id,recurso,disponible,clase_id ,cantidad)  VALUES(1, 'libro de economia 1',true, 1,1);");
-        stmt.execute("INSERT INTO Recurso (id,recurso,disponible,clase_id ,cantidad)  VALUES(2, 'libro de economia 1',true, 1,1);");
-
+        stmt.execute("INSERT INTO Recurso (id,recurso,disponible ,cantidad)  VALUES(1, 'libro de economia 1',true,1);");
+        stmt.execute("INSERT INTO Recurso (id,recurso,disponible,cantidad)  VALUES(2, 'libro de economia 1',true,1);");
+        stmt.execute("INSERT INTO RecursoClase (clase_id,recurso_id,cantidadUSE)  VALUES(1,1,10);");
+        stmt.execute("INSERT INTO RecursoClase (clase_id,recurso_id,cantidadUSE)  VALUES(1,2,10);");
         ServiciosReporte report=ServiciosReporteFactory.getInstance().getServiciosReporteForTesting();
         assertTrue("No deben aparecer recursos de un periodo que no exista (2016-1)", report.consultarRecursosXperiodo("2016-1").isEmpty());
         
--- tables.sql
+++ tables.sql
@@ -1,5 +1,5 @@
--- Created by Vertabelo (http://vertabelo.com)
--- Last modification date: 2017-04-29 02:24:01.63
+--- Created by Vertabelo (http://vertabelo.com)
+-- Last modification date: 2017-05-02 22:08:34.818
 
 -- tables
 -- Table: Asignatura
@@ -48,7 +48,6 @@ CREATE TABLE MateriaCohorte (
     Materia_sigla varchar(5)  NOT NULL,
     Cohorte_id int  NOT NULL,
     Profesor_documento int  NOT NULL
-    
 );
 
 -- Table: Periodo
@@ -57,7 +56,6 @@ CREATE TABLE Periodo (
     fecha_inicio date  NOT NULL,
     fecha_fin date  NOT NULL
 );
-
 -- Table: Posgrado
 CREATE TABLE Posgrado (
     id int  NOT NULL,
@@ -92,8 +90,14 @@ CREATE TABLE Recurso (
     id int  NOT NULL,
     recurso varchar(100)  NOT NULL,
     disponible boolean  NOT NULL,
-    cantidad int  NOT NULL,
-    Clase_id int  NOT NULL
+    cantidad int  NOT NULL
+);
+
+-- Table: RecursoClase
+CREATE TABLE RecursoClase (
+    Clase_id int  NOT NULL,
+    Recurso_id int  NOT NULL,
+    cantidadUSE int  NOT NULL
 );
 
 -- Table: Salon
@@ -102,18 +106,18 @@ CREATE TABLE Salon (
     Clase_id int  NOT NULL
 );
 
--- Table: rol
-CREATE TABLE rol (
-    rol varchar(30)  NOT NULL
-);
-
--- Table: usr
-CREATE TABLE usr (
-    usuario int  NOT NULL,
+-- Table: Usr
+CREATE TABLE Usr (
+    usuario varchar(10)  NOT NULL,
     pwd varchar(100)  NOT NULL,
     salt varchar(5)  NOT NULL,
     nombre varchar(50)  NOT NULL,
-	rol varchar(30)  NOT NULL
+    rol varchar(30)  NOT NULL
+);
+
+-- Table: rol
+CREATE TABLE rol (
+    rol varchar(30)  NOT NULL
 );
 
 -- Table: usrXrol
@@ -133,6 +137,7 @@ ALTER TABLE Profesor ADD CONSTRAINT Profesor_pk PRIMARY KEY (documento);
 ALTER TABLE Salon ADD CONSTRAINT Salon_pk PRIMARY KEY (Clase_id);
 ALTER TABLE rol ADD CONSTRAINT rol_pk PRIMARY KEY (rol);
 ALTER TABLE usr ADD CONSTRAINT usr_pk PRIMARY KEY (usuario);
+ALTER TABLE RecursoClase ADD CONSTRAINT RecursoClase_pk PRIMARY KEY (Clase_id,Recurso_id);
 --ALTER TABLE usrXrol ADD CONSTRAINT usrXrol_pk PRIMARY KEY (rol_rol,usr_usuario);
 ALTER TABLE Periodo ADD CONSTRAINT Periodo_pk PRIMARY KEY (periodo);
 ALTER TABLE PrerequisitoMateria ADD CONSTRAINT PrerequisitoMateria_pk PRIMARY KEY (materia_sigla,prerrequisito_sigla);
@@ -142,7 +147,7 @@ ALTER TABLE MateriaCohorte ADD CONSTRAINT MateriaCohorte_pk PRIMARY KEY (Materia
 -- unique keys
 ALTER TABLE Clase ADD CONSTRAINT uk_clase UNIQUE (id) ;
 ALTER TABLE Profesor ADD CONSTRAINT Profesor_ak_1 UNIQUE (correo) ;
--- foreign keys
+
 
 -- foreign keys
 -- Reference: Asignatura_Posgrado (table: Asignatura)
@@ -154,13 +159,13 @@ ALTER TABLE Asignatura ADD CONSTRAINT Asignatura_Posgrado
 -- Reference: Clase_MateriaCohorte (table: Clase)
 ALTER TABLE Clase ADD CONSTRAINT Clase_MateriaCohorte
     FOREIGN KEY (MateriaCohorte_Materia_sigla, MateriaCohorte_Cohorte_id, MateriaCohorte_Profesor_documento)
-    REFERENCES MateriaCohorte (Materia_sigla, Cohorte_id, Profesor_documento)
+    REFERENCES MateriaCohorte (Materia_sigla, Cohorte_id, Profesor_documento) 
 ;
 
 -- Reference: Cohorte_Periodo (table: Cohorte)
 ALTER TABLE Cohorte ADD CONSTRAINT Cohorte_Periodo
     FOREIGN KEY (Periodo_periodo)
-    REFERENCES Periodo (periodo) 
+    REFERENCES Periodo (periodo)  
 ;
 
 -- Reference: MateriaCohorte_Cohorte (table: MateriaCohorte)
@@ -178,7 +183,7 @@ ALTER TABLE MateriaCohorte ADD CONSTRAINT MateriaCohorte_Materia
 -- Reference: MateriaCohorte_Profesor (table: MateriaCohorte)
 ALTER TABLE MateriaCohorte ADD CONSTRAINT MateriaCohorte_Profesor
     FOREIGN KEY (Profesor_documento)
-    REFERENCES Profesor (documento)
+    REFERENCES Profesor (documento)  
 ;
 
 -- Reference: Materia_Asignatura (table: Materia)
@@ -196,7 +201,7 @@ ALTER TABLE PrerequisitoMateria ADD CONSTRAINT PrerequisitoMateria_Materia
 -- Reference: PrerequisitoMateria_Materia2 (table: PrerequisitoMateria)
 ALTER TABLE PrerequisitoMateria ADD CONSTRAINT PrerequisitoMateria_Materia2
     FOREIGN KEY (prerrequisito_sigla)
-    REFERENCES Materia (sigla)  
+    REFERENCES Materia (sigla) 
 ;
 
 -- Reference: ProfesorXHorario_Horario (table: ProfesorXHorario)
@@ -208,20 +213,27 @@ ALTER TABLE ProfesorXHorario ADD CONSTRAINT ProfesorXHorario_Horario
 -- Reference: ProfesorXHorario_Profesor (table: ProfesorXHorario)
 ALTER TABLE ProfesorXHorario ADD CONSTRAINT ProfesorXHorario_Profesor
     FOREIGN KEY (Profesor_documento)
-    REFERENCES Profesor (documento)  
-;
-
--- Reference: Recurso_Clase (table: Recurso)
-ALTER TABLE Recurso ADD CONSTRAINT Recurso_Clase
-    FOREIGN KEY (Clase_id)
-    REFERENCES Clase (id)  
+    REFERENCES Profesor (documento) 
 ;
 
 -- Reference: Salon_Clase (table: Salon)
 ALTER TABLE Salon ADD CONSTRAINT Salon_Clase
     FOREIGN KEY (Clase_id)
-    REFERENCES Clase (id)
+    REFERENCES Clase (id)  
 ;
+
+-- Reference: Table_33_Clase (table: RecursoClase)
+ALTER TABLE RecursoClase ADD CONSTRAINT Table_33_Clase
+    FOREIGN KEY (Clase_id)
+    REFERENCES Clase (id) 
+;
+
+-- Reference: Table_33_Recurso (table: RecursoClase)
+ALTER TABLE RecursoClase ADD CONSTRAINT Table_33_Recurso
+    FOREIGN KEY (Recurso_id)
+    REFERENCES Recurso (id) 
+;
+
 -- Reference: usr_rol (table: Usr)
 ALTER TABLE Usr ADD CONSTRAINT usr_rol
     FOREIGN KEY (rol)
